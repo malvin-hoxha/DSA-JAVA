e1f33455a79671659a073e04990aedd00cd9ce36
malvin-hoxha <malvinhodev@gmail.com>
2026-09-22 16:53:59 +0300

    Implementation of Recursive Binary search in Java

--- Algorithms.java
+++ Algorithms.java
@@ -48,13 +48,41 @@ class BinarySearch {
     }
 }
 
+class BinarySearchRecursive {
+
+    public int search(int[] arr, int target, int start, int end) {
+
+        if (start <= end) {
+            int m = (start + end) / 2;
+
+            if (arr[m] == target) {
+                return m;
+            } else if (arr[m] < target) {
+                return search(arr, target, m + 1, end);
+            } else {
+                return search(arr, target, start, m - 1);
+            }
+        }
+
+        return -1;
+    }
+
+    public void print(int[] arr, int result) {
+        if (result != -1) {
+            System.out.println(arr[result] + " is found at index " + result);
+        } else {
+            System.out.println("Target not found");
+        }
+    }
+}
+
 public class Algorithms {
 
     public static void main(String[] args) {
 
-        BinarySearch bSearch = new BinarySearch();
+        BinarySearchRecursive bSearch = new BinarySearchRecursive();
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        int result = bSearch.search(arr, 7);
+        int result = bSearch.search(arr, 7, 0, arr.length - 1);
         bSearch.print(arr, result);
 
     }
