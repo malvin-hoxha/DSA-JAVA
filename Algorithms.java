a87644846b136f6922a9c69b0cb1638a5229f647
malvin-hoxha <malvinhodev@gmail.com>
2026-09-22 16:43:11 +0300

    Implementation of Binary search in Java

--- Algorithms.java
+++ Algorithms.java
@@ -9,20 +9,53 @@ class LinearSearch {
 
         return -1;
     }
+
+    public void print(int[] arr, int result) {
+        if (result != -1) {
+            System.out.println(arr[result] + " is found at index " + result);
+        } else {
+            System.out.println("Target not found");
+        }
+    }
+}
+
+class BinarySearch {
+
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int m = (start + end) / 2;
+            if (arr[m] == target) {
+                return m;
+            } else if (arr[m] < target) {
+                start = m + 1;
+            } else {
+                end = m - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public void print(int[] arr, int result) {
+        if (result != -1) {
+            System.out.println(arr[result] + " is found at index " + result);
+        } else {
+            System.out.println("Target not found");
+        }
+    }
 }
 
 public class Algorithms {
 
     public static void main(String[] args) {
 
-        int[] arr = { 5, 6, 90, 85, 15 };
-        LinearSearch ls = new LinearSearch();
-        int result = ls.search(arr, 85);
+        BinarySearch bSearch = new BinarySearch();
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int result = bSearch.search(arr, 7);
+        bSearch.print(arr, result);
 
-        if (result != -1) {
-            System.out.println(arr[result] + " is found at index " + result);
-        } else {
-            System.out.println("Target not found");
-        }
     }
 }
