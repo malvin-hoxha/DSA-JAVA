b3c35b4f4e8bfabd66a6126fef2356f9086e5801
malvin-hoxha <malvinhodev@gmail.com>
2026-09-23 18:37:03 +0300

    Implementing MergeSort algorithm in Java

--- Algorithms.java
+++ Algorithms.java
@@ -167,6 +167,55 @@ class QuickSort {
     }
 }
 
+class MergeSort {
+
+    public void sort(int[] arr, int low, int high) {
+
+        if (low >= high) {
+            return;
+        }
+        int mid = (low + high) / 2;
+
+        sort(arr, low, mid);
+        sort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public void merge(int[] arr, int low, int mid, int high) {
+
+        int[] temp = new int[high - low + 1];
+        int i = low, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+        }
+
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j <= high) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < temp.length; l++) {
+            arr[low + l] = temp[l];
+        }
+    }
+}
+
 public class Algorithms {
 
     public static void main(String[] args) {
@@ -179,8 +228,8 @@ public class Algorithms {
 
         System.out.println();
 
-        QuickSort quickSort = new QuickSort();
-        quickSort.sort(arr, 0, arr.length - 1);
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(arr, 0, arr.length - 1);
 
         for (int i : arr) {
             System.out.print(i + " ");
